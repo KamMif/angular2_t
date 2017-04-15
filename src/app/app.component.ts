fc12be89bd169c2470cfaebe981ab09f689db5ae
kam <kammif1990@gmail.com>
2017-04-15 17:53:01 +0500

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  hero: Hero = {
+    id: 1,
+    name: "WindStorm"
+  }
+}
+
+export class Hero {
+  id: number;
+  name: string;
 }
